4738eb291c05a4db5700e7ac03af04c6a651e544
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-08-03 21:22:07 +0530

    .

--- L1Solution-ex3.sol
+++ L1Solution-ex3.sol
@@ -6,19 +6,32 @@ pragma solidity 0.8.0;
 contract L1Solution{
 
     address public STARKNET_CORE = 0xde29d060D45901Fb19ED6C6e959EB22d8626708e;
-    uint256 public L2_EVALUATOR_ADDRESS = 0x595bfeb84a5f95de3471fc66929710e92c12cce2b652cd91a6fef4c5c09cd99;
+    // uint256 public L2_EVALUATOR_ADDRESS = 0x595bfeb84a5f95de3471fc66929710e92c12cce2b652cd91a6fef4c5c09cd99;
     
-    function consumeMessage(uint _l2WalletAddr) public {
+    function solve(address _coreAddress, uint _l2Contract, uint _selectorUint, uint _l2Address) public {
+        uint256[] memory payload = new uint256[](1);
+        payload[0] = _l2Address;
+
+        IStarknetCore(_coreAddress).sendMessageToL2(
+            _l2Contract,
+            _selectorUint,
+            payload
+        );
+    }
+
+    function consumeMessage(uint L2_EVALUATOR_ADDRESS, uint _l2WalletAddr) public {
         uint[] memory payload = new uint[](1);
         payload[0] = _l2WalletAddr;
 
         IStarknetCore(STARKNET_CORE).consumeMessageFromL2(
-            uint256(L2_EVALUATOR_ADDRESS),
+            L2_EVALUATOR_ADDRESS,
             payload
         );
     }
 
 }
+
+// 2526149038677515265213650328426051013974292914551952046681512871525993794969, 3003102924212698328615085830418988004655054268051673979130181410315737315214
 
 
 
@@ -37,4 +50,3 @@ interface IStarknetCore {
 
     function l2ToL1Messages(bytes32 msgHash) external view returns (uint256);
 }
-

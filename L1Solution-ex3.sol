c66a6e9b1124d6ee7960ee28db21344faeda3ccc
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-08-03 20:50:39 +0530

    ex3 unsolvable

--- L1Solution-ex3.sol
+++ L1Solution-ex3.sol
@@ -1,13 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
-import "./IStarknetCore.sol";
-
+// import "./IStarknetCore.sol";
 
 contract L1Solution{
-    function consumeMessage(uint _l2Address){
-        uint[] payload = new uint[](1);
-        
+
+    address public STARKNET_CORE = 0xde29d060D45901Fb19ED6C6e959EB22d8626708e;
+    uint256 public L2_EVALUATOR_ADDRESS = 0x595bfeb84a5f95de3471fc66929710e92c12cce2b652cd91a6fef4c5c09cd99;
+    
+    function consumeMessage(uint _l2WalletAddr) public {
+        uint[] memory payload = new uint[](1);
+        payload[0] = _l2WalletAddr;
+
+        IStarknetCore(STARKNET_CORE).consumeMessageFromL2(
+            uint256(L2_EVALUATOR_ADDRESS),
+            payload
+        );
     }
 
 }
+
+
+
+interface IStarknetCore {
+    function sendMessageToL2(
+        uint256 to_address,
+        uint256 selector,
+        uint256[] calldata payload
+    ) external returns (bytes32);
+
+    
+    function consumeMessageFromL2(
+        uint256 fromAddress,
+        uint256[] calldata payload
+    ) external returns (bytes32);
+
+    function l2ToL1Messages(bytes32 msgHash) external view returns (uint256);
+}
+
